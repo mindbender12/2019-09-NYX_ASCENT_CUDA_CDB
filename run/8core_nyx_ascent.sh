e193fdb1703871cf1eec21f17362ce14b6a61e45
Quincy Wofford <qwofford@login5.summit.olcf.ornl.gov>
2019-09-19 19:51:32 -0400

    modified scripts to sample and generate cinema database

--- run/8core_nyx_ascent.sh
+++ run/8core_nyx_ascent.sh
@@ -3,10 +3,11 @@
 #BSUB -J nyx_in_situ
 #BSUB -nnodes 8
 #BSUB -P CSC340
-#BSUB -W 00:03
+#BSUB -W 00:07
 
 module load gcc/5.4.0
-module load python/2.7.15-anaconda2-5.3.0
+module load spectrum-mpi
 module load hdf5/1.10.3
+module load cuda/10.1.105
 
-jsrun -n 8 <basepath>/opt/nyx/src/Exec/LyA/Nyx3d.gnu.PROF.MPI.OMP.ex inputs
+jsrun -n 8 -g 1  <basepath>/opt/nyx/src/Exec/LyA/Nyx3d.gnu.PROF.MPI.OMP.ex inputs
--- validate/check_one_output_file.sh
+++ validate/check_one_output_file.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-source ./env.sh
-start=$(date +%s)
-while [ ! -d ${BASE_PATH}/opt/nyx/src/Exec/LyA/bp_example_00000 ]
-do
-reset
-echo "Your job hasn't yet started generating output. Checking job status..."
-jobstat -u $(whoami)
-echo ==============================================
-echo  Elapsed time $(( $(date +%s) - ${start} )) seconds 
-echo ==============================================
-sleep 5
-if [ "$(( $(date +%s) - ${start} ))" -gt "600" ]; then
-  echo Job ran over 10 minutes. Should run \< 3min.
-  echo Recommend aborting job listed above.
-  exit 1
-fi
-done
-reset
-echo Validation := \"At least one valid output file is generated.\"
-echo SUCCESS
-echo Output location: ${BASE_PATH}/opt/nyx/src/Exec/LyA/
-echo ====================================================
-echo all compiled software will persist in the OPT_PATH
-echo BASE_PATH = ${BASE_PATH}/opt
-echo ====================================================
+#source ./env.sh
+#start=$(date +%s)
+#while [ ! -d ${BASE_PATH}/opt/nyx/src/Exec/LyA/bp_example_00000 ]
+#do
+#reset
+#echo "Your job hasn't yet started generating output. Checking job status..."
+#jobstat -u $(whoami)
+#echo ==============================================
+#echo  Elapsed time $(( $(date +%s) - ${start} )) seconds 
+#echo ==============================================
+#sleep 5
+#if [ "$(( $(date +%s) - ${start} ))" -gt "600" ]; then
+#  echo Job ran over 10 minutes. Should run \< 3min.
+#  echo Recommend aborting job listed above.
+#  exit 1
+#fi
+#done
+#reset
+#echo Validation := \"At least one valid output file is generated.\"
+#echo SUCCESS
+#echo Output location: ${BASE_PATH}/opt/nyx/src/Exec/LyA/
+#echo ====================================================
+#echo all compiled software will persist in the OPT_PATH
+#echo BASE_PATH = ${BASE_PATH}/opt
+#echo ====================================================
